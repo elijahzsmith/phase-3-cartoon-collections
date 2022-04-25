35099488d54bb001121d578d95a65941e83ebecb
Elijah Smith <elijahsmith.m@gmail.com>
2022-04-25 17:00:31 -0500

    commit tree 3b8a3db1daacfaaaea836c579566676e3adb2649

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves dwarves_array
+  dwarves_array.each.with_index(0) do |dwarf, index|
+    print "#{index+1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planeteer_calls
+  planeteer_calls.map do |call|
+    call.capitalize + '!'
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls calls_array
+  calls_array.any? do |call| call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese str_arr
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.find do |cheese_type|
+    str_arr.include? cheese_type 
+  end
 end
